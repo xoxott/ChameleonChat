bba6339e7eef873838f96de61fd715d0e6a0d235
yang <212920320@qq.com>
2026-01-30 20:32:50 +0800

    Update TypeScript configuration to disable unused locals and parameters checks, streamline GitHub Actions build step by removing logging, and clean up imports in EncryptTab component.

--- src/components/EncryptTab.tsx
+++ src/components/EncryptTab.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import { 
   formatTimeFromTimestamp, 
   getExpiryTimeFromTimestamp, 
-  getCurrentTimeSlot, 
   isExpiredFromTimestamp, 
   getTimeRemainingFromTimestamp 
 } from '../constants'
